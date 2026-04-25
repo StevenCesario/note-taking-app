497a3e150b0016bec29a2f9522fd2836822d10bd
StevenCesario <steven.cesario@proton.me>
2026-04-25 09:16:40 +0200

    init commit :)

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import NoteTakingApp from './components/NoteTakingApp'
+
+function App() {
+
+  return (
+    <NoteTakingApp />
+  )
+}
+
+export default App
---
+++ src/components/NoteTakingApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoteTakingApp = () => {
+  return (
+    <div>NoteTakingApp</div>
+  )
+}
+
+export default NoteTakingApp
