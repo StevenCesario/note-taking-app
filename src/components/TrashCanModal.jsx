00886de0c22e7ca1a8cb66f3c748fb3e9e28b300
StevenCesario <steven.cesario@proton.me>
2026-04-26 12:06:08 +0200

    `key={note.id}` added to the Note component in the trash can modal component

--- src/components/TrashCanModal.jsx
+++ src/components/TrashCanModal.jsx
@@ -9,7 +9,7 @@ const TrashCanModal = ({ notes, onClose, onRestore, onPermaDelete }) => {
         {/* It also just clicked here; I believe we're gonna FIRSTS filter based on isActive and THEN map the NEW array to use the Note component? */}
         {/* NOW  with 1. the isActive boolean implemented across the system and 2. the functions for onRestore and onPermaDelete created, we SHOULD be able to do... */}
         {/* For the filtering, we want a new filtered array where isActive is... false */}
-        {notes.filter(note => note.isActive === false).map(note => <Note note={note} onRestore={onRestore} onPermaDelete={onPermaDelete} />)}
+        {notes.filter(note => note.isActive === false).map(note => <Note key={note.id} note={note} onRestore={onRestore} onPermaDelete={onPermaDelete} />)}
         {/* Will this chained expression work???????? */}
         {/* IT DOES! But the "trashed" note is still visible in the active view haha, we need to filter there for isActive to be true */}
       </div>
